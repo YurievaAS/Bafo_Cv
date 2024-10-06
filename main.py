818476fd36ab59c844e19554caf3b1cb35bdf2f4
arish <arishxoxo25@gmail.com>
2024-10-06 14:12:43 +0300

    Отслеживание рук

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import cv2
+import numpy as np
 import matplotlib as mlp
-
+import os
+import time
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import mediapipe as mp
 cap = cv2.VideoCapture(0) #считывание данных с камеры
-cap.set(3, 300) # 3- код ширины в qданной ф-ии, устанавливаем ширину 300
-cap.set(4, 500) # 4- высоты ширины в данной ф-ии, устанавливаем высоту 500
+#cap.set(3, 300)
+#cap.set(4, 500)
+mpHands = mp.solutions.hands #доступ к функциональности, связанной с обнаружением рук
+hands = mpHands.Hands() #создание объекта на основе класса
+mpDraw = mp.solutions.drawing_utils #доступ к функциональности
 
+pTime = 0
+cTime = 0
 while True:
     success, img = cap.read()
-    #видео - последовательность изображений (в success (bool) записывается, удалось ли считать изображение)
-    cv2.imshow ('Result', img)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
+    #results.multi_hand_landmarks - найдены руки или нет
+    if (results.multi_hand_landmarks != None):
+        for hand_lm in results.multi_hand_landmarks:
+            for id, lm in enumerate(hand_lm.landmark):
+                height, width, channels = img.shape
+                x, y = int(lm.x*width), int(lm.y*height) #преобразование landmarks в координаты
+                print(id," :",  x, y)
+            mpDraw.draw_landmarks(img, hand_lm, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_DUPLEX,3,(125,67,234),10 )
+
+    cv2.imshow('Video', img)
 
     if (cv2.waitKey(1) & 0xFF == ord('q')):
         break
-    #ждет 1 миллисекунду нажатия клавиши. Если нажата клавиша 'q', цикл прерывается
