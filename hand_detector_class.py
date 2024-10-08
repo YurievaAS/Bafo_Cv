0d4ace11ea4e5e6e2f33863cf156aa193d82442e
arish <arishxoxo25@gmail.com>
2024-10-08 11:27:02 +0300

    Класс hand_detector

---
+++ hand_detector_class.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+import matplotlib as mlp
+import os
+import time
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import mediapipe as mp
+
+class HandDetector():
+    #конструктор класса (все сложности можно убрать)
+    def __init__(self, mode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, trackCon=0.5):
+        self.pTime = 0
+        #параметры модели
+        self.mode = mode
+        self.maxHands = maxHands
+        self.modelComplexity = modelComplexity
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+
+        self.mpHands = mp.solutions.hands #доступ к функциональности
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplexity,\
+                                        self.detectionCon, self.trackCon) # создание объекта
+        self.mpDraw = mp.solutions.drawing_utils #доступ к функциональности
+
+    #метод класса, отвечающий за отображение
+    def find_hands(self, img, draw=True, fps=True,show=True):
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(img_rgb)
+        if (results.multi_hand_landmarks):
+            for hand_lm in results.multi_hand_landmarks:
+                if (draw):
+                    self.mpDraw.draw_landmarks(img, hand_lm, self.mpHands.HAND_CONNECTIONS)
+        if (fps):
+            cTime = time.time()
+            fps = 1 / (cTime - self.pTime)
+            self.pTime = cTime
+            cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_DUPLEX, 3, (125, 67, 234), 10)
+        if (show):
+            cv2.imshow("Hand Detector", img)
+        return img
+
+    #метод класса, вовзращающий за коор-ты точек (id на руке - номер в массиве)
+    def hand_position(self, img, handNo = 0):
+        lmList = []
+        cx = 0
+        cy = 0
+        height, width, channels = img.shape
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(img_rgb)
+        if (self.results.multi_hand_landmarks):
+            if (handNo < len(self.results.multi_hand_landmarks)):
+                myHand = self.results.multi_hand_landmarks[handNo]
+                for id, lm in enumerate(myHand.landmark):
+                    lmList.append([int(lm.x * width),int(lm.y* height)])
+        return lmList
+
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import hand_detector_class as hdc
 import cv2
 import numpy as np
 import matplotlib as mlp
@@ -5,34 +6,15 @@ import os
 import time
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import mediapipe as mp
-cap = cv2.VideoCapture(0) #считывание данных с камеры
-#cap.set(3, 300)
-#cap.set(4, 500)
-mpHands = mp.solutions.hands #доступ к функциональности, связанной с обнаружением рук
-hands = mpHands.Hands() #создание объекта на основе класса
-mpDraw = mp.solutions.drawing_utils #доступ к функциональности
 
-pTime = 0
-cTime = 0
+detector = hdc.HandDetector()
+cap = cv2.VideoCapture(0)
+
 while True:
     success, img = cap.read()
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(img_rgb)
-    #results.multi_hand_landmarks - найдены руки или нет
-    if (results.multi_hand_landmarks != None):
-        for hand_lm in results.multi_hand_landmarks:
-            for id, lm in enumerate(hand_lm.landmark):
-                height, width, channels = img.shape
-                x, y = int(lm.x*width), int(lm.y*height) #преобразование landmarks в координаты
-                print(id," :",  x, y)
-            mpDraw.draw_landmarks(img, hand_lm, mpHands.HAND_CONNECTIONS)
-
-    cTime = time.time()
-    fps = 1/(cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_DUPLEX,3,(125,67,234),10 )
-
-    cv2.imshow('Video', img)
-
-    if (cv2.waitKey(1) & 0xFF == ord('q')):
+    if (not success):
         break
+    detector.find_hands(img)
+    print(0, detector.hand_position(img)[0:5])
+    print(1, detector.hand_position(img, 1)[0:5]) #показывает 1-е 5 элементов, чтобы загружено не было
+
