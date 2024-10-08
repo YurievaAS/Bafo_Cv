b4490dc4ded1f6433c6cdf35e06f2e956c231425
arish <arishxoxo25@gmail.com>
2024-10-08 11:44:17 +0300

    Класс hand_detector #1

--- hand_detector_class.py
+++ hand_detector_class.py
@@ -24,7 +24,7 @@ class HandDetector():
         self.mpDraw = mp.solutions.drawing_utils #доступ к функциональности
 
     #метод класса, отвечающий за отображение
-    def find_hands(self, img, draw=True, fps=True,show=True):
+    def find_hands(self, img, draw=True, fps=True):
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = self.hands.process(img_rgb)
         if (results.multi_hand_landmarks):
@@ -36,8 +36,6 @@ class HandDetector():
             fps = 1 / (cTime - self.pTime)
             self.pTime = cTime
             cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_DUPLEX, 3, (125, 67, 234), 10)
-        if (show):
-            cv2.imshow("Hand Detector", img)
         return img
 
     #метод класса, вовзращающий за коор-ты точек (id на руке - номер в массиве)
--- main.py
+++ main.py
@@ -14,7 +14,8 @@ while True:
     success, img = cap.read()
     if (not success):
         break
-    detector.find_hands(img)
+    cv2.imshow("Hand Detector", detector.find_hands(img))
     print(0, detector.hand_position(img)[0:5])
     print(1, detector.hand_position(img, 1)[0:5]) #показывает 1-е 5 элементов, чтобы загружено не было
-
+    if (cv2.waitKey(1) & 0xFF == ord('q')):
+        break
