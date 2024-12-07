40dc02800fd07ea95d067db59ddff37d77a3492b
Mikhail <kantyt3@gmail.com>
2024-12-07 03:02:36 +0300

    Added project files

--- main.py
+++ main.py
@@ -1,40 +1,59 @@
-import cv2 
-import numpy as np
-import track as track
-import tensorflow
-from tensorflow import keras
+import sys
 import time
+import cv2
+import numpy as np
+from PyQt5.QtWidgets import QApplication
+from tensorflow import keras
+import track
+import ui
 
+# Список слов для предсказания
+words = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'help'
+]
+
+def to_numpy_array(img):
+    """Преобразует изображение в numpy массив."""
+    return np.array(img).reshape(1, 32, 32, 1)
+
+# Инициализация трекера рук и камеры
 tracker = track.HandsTrack()
 cam_ = cv2.VideoCapture(0)
 
-empty_img = np.zeros(10)
-
-success,img = cam_.read()
-
-model = keras.models.load_model('models/model.keras')
-
-def to_numpy_array(img):
-    return np.array(img).reshape(1,28,28,1)
+# Загрузка модели
+model = keras.models.load_model('/Users/mishgun/Documents/Python/ML Tensorflow/BaFo/final/ui/model_32.keras')
 prediction = None
 
+# Создание экземпляра приложения и главного окна
+app = QApplication(sys.argv)
+window = ui.MainWindow()
+window.show()  # Отображение окна
+
 while True:
-    success, img = cam_.read()
-    if (not success):
+    success, img = cam_.read()  # Чтение кадра из камеры
+    
+    if not success:
         print("Capture not found!")
         break
-    try:
-        tracker.isEmpty = False
-        img = tracker.tracking(img)
-        cv2.imshow('Hand tracker', img)
-    except:
-        tracker.isEmpty = True
-        cv2.imshow('Hand tracker', empty_img)
 
-    if tracker.isEmpty == False:
-        prediction = model.predict(to_numpy_array(img)) 
-        print(prediction)
-        
-    if (cv2.waitKey(1) & 0xFF == ord('q')):
+    # Обновление кадра в окне
+    window.update_frame(img)
+
+    # Трекинг рук на изображении
+    img = tracker.tracking(img)
+
+    if not tracker.isEmpty:
+        prediction = model.predict(to_numpy_array(img))  # Предсказание
+        predicted_word = np.argmax(prediction)  # Получение индекса предсказанного слова
+        window.set_text(words[predicted_word])  # Установка текста в окно (используем список слов)
+
+    # Выход из цикла при нажатии клавиши 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    time.sleep(0.1)
+
+    time.sleep(0.1)  # Задержка для управления частотой кадров
+
+# Освобождение ресурсов после завершения работы
+cam_.release()
+cv2.destroyAllWindows()
--- track.py
+++ track.py
@@ -16,31 +16,46 @@ class HandsTrack:
         self.hands = self.mp_hands.Hands(self.mode, self.maxHands,self.model_complexity, self.min_detection_confidence, self.min_tracking_confidence)
     
         self.isEmpty = True
-
+    
     def tracking(self, img):
         h, w = img.shape[0:2]
         x, y = 0,0
         crop_size = 20
         self.results = self.hands.process(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+
         if self.results.multi_hand_landmarks:
             for landmark_iter in self.results.multi_hand_landmarks:
+                x_coords = [landmark.x for landmark in landmark_iter.landmark]
+                y_coords = [landmark.y for landmark in landmark_iter.landmark]
+
+                black_image = np.zeros_like(img)
+
+                for landmark in landmark_iter.landmark:
+                    h_, w_, _ = black_image.shape
+                    x = int(landmark.x * w_) 
+                    y = int(landmark.y * h_) 
+                    cv2.circle(black_image, (x, y), 5, (255, 255, 255), -1)
+
+                # Определяем границы квадрата
+                x_min = int(min(x_coords) * img.shape[1]) - crop_size
+                x_max = int(max(x_coords) * img.shape[1]) + crop_size
+                y_min = int(min(y_coords) * img.shape[0]) - crop_size
+                y_max = int(max(y_coords) * img.shape[0]) + crop_size
                 
-                x_min = min(int(landmark.x * img.shape[1]) for landmark in landmark_iter.landmark) - crop_size
-                x_max = max(int(landmark.x * img.shape[1]) for landmark in landmark_iter.landmark) + crop_size
-                y_min = min(int(landmark.y * img.shape[0]) for landmark in landmark_iter.landmark) - crop_size
-                y_max = max(int(landmark.y * img.shape[0]) for landmark in landmark_iter.landmark) + crop_size
-                
-                # _min_ = min(y_min,x_min)
-                # _max_ = max(y_max,x_max)
-                # img = img[_min_:_max_,_min_:_max_]
-                img = img[y_min:y_max,x_min:x_max]
-                img = cv2.resize(img,(28,28), interpolation=cv2.INTER_LINEAR)
-                img = cv2.cvtColor(img[: ,::-1],cv2.COLOR_RGB2GRAY)
+                black_image = black_image[y_min : y_max,x_min : x_max]
+                try:
+                    black_image = cv2.resize(black_image,(32,32), interpolation=cv2.INTER_NEAREST)
+                except:
+                    self.isEmpty = True
+                    return img
+                black_image = cv2.cvtColor(black_image[: ,::-1],cv2.COLOR_RGB2GRAY)
                 self.isEmpty = False
-                return img
+                return black_image
         self.isEmpty = True
         return img
     
+    
+
         
 
 
